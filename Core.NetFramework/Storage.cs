4fbb4808ceaecac46e9ae3867cfc69fadbdfba3c
Paweł Stepnowski <fdafadf@gmail.com>
2020-03-05 21:55:16 +0100

    Storage dla DotNetFramework i DotNetStandard.

---
+++ Core.NetFramework/Storage.cs
@@ -0,0 +1,43 @@
+﻿using Core.NetStandard;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.NetFramework
+{
+    public class Storage : IStorage
+    {
+        public static readonly Storage Instance = new Storage();
+
+        protected Storage()
+        {
+        }
+
+        public T Read<T>(string resourceName)
+        {
+            return JsonConvert.DeserializeObject<T>(File.ReadAllText(GetResourceFilePath(resourceName)));
+        }
+
+        public void Write<T>(string resourceName, T resource)
+        {
+            File.WriteAllText(GetResourceFilePath(resourceName), JsonConvert.SerializeObject(resource));
+        }
+
+        public string GetResourceFilePath(string resourceName)
+        {
+            return Path.Combine(ResourceDirectoryPath, resourceName);
+        }
+
+        public string ResourceDirectoryPath
+        {
+            get
+            {
+                return Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            }
+        }
+    }
+}
---
+++ Core.NetStandard/IStorage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Core.NetStandard
+{
+    public interface IStorage
+    {
+        T Read<T>(string resourceName);
+        void Write<T>(string resourceName, T resource);
+    }
+}
